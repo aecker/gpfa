1861df9e157a890c2b32eb70628b2f0b95862f71
Alexander Ecker <alexander.ecker@uni-tuebingen.de>
2012-10-29 17:52:30 -0500

    Added fast Toeplitz matrix inversion to the repo

---
+++ startup.m
@@ -0,0 +1,7 @@
+function startup()
+% Add folders that are needed to MATLAB path
+% AE 2012-10-29
+
+folder = fileparts(mfilename('fullpath'));
+addpath(folder)
+addpath(fullfile(folder, 'invToeplitz'));
